7bdb2c243cebf8154e3eaf22a1886feedeb59e41
Jake Garcia <bballwiz4@gmail.com>
2020-02-19 18:49:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
     return false
   end
 
-  WIN_COMBINATIONS.each do |win_combo|
+  WIN_COMBINATIONS.select do |win_combo|
     if win_combo[0] == "X" && win_combo[1] == "X" && win_combo[2] == "X"
       return win_combo
     elsif win_combo[0] == "O" && win_combo[1] == "O" && win_combo[2] == "O"
